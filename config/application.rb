78adec288582f41ec8bc5ffe95f668f87cccb973
Shintani Teppei <teppest1215@gmail.com>
2017-08-29 11:47:35 +0900

    i18nの導入

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ module Ittern
     # config.time_zone = 'Central Time (US & Canada)'
     config.time_zone = 'Tokyo'
 
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
