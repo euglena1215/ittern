c6c6c435d4906e5b8bc0f9ec471b34ad3078d7d5
Shintani Teppei <teppest1215@gmail.com>
2017-08-31 23:16:03 +0900

    Model.pluckを外に出した

--- db/fixtures/03_review.rb
+++ db/fixtures/03_review.rb
@@ -8,13 +8,16 @@ contents = %w[
   一人で開発してた方がましな
 ]
 
+user_ids = User.pluck(:id)
+company_ids = Company.pluck(:id)
+
 (1..40).each do |i|
   Review.seed do |r|
     r.id = i
     r.content = "#{contents.sample}インターンでした！\n#{ganre.sample}好きには特にオススメ！"
-    r.user = User.find(User.pluck(:id).sample)
+    r.user = User.find(user_ids.sample)
     r.url = rand(2).odd? ? "http://hoge.com/intern" : nil
-    r.company = Company.find(Company.pluck(:id).sample)
+    r.company = Company.find(company_ids.sample)
     r.rate = (1..5).to_a.sample
   end
 end
