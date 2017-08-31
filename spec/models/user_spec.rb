fbf847e5398755d4ae79f8455c741a6bdff4ae56
Shintani Teppei <teppest1215@gmail.com>
2017-08-31 13:00:56 +0900

    usersにnameを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,6 +19,7 @@
 #  unconfirmed_email      :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
+#  name                   :string
 #
 # Indexes
 #
