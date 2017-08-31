ac483d35a2e8eb89cde3e857bbca46df710793f8
Shintani Teppei <teppest1215@gmail.com>
2017-08-31 20:50:59 +0900

    viewをざっくり修正

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -26,39 +26,28 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
 
-    respond_to do |format|
-      if @review.save
-        format.html { redirect_to @review, notice: "Review was successfully created." }
-        format.json { render :show, status: :created, location: @review }
-      else
-        format.html { render :new }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
-      end
+    if @review.save
+      redirect_to @review, notice: "Review was successfully created."
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /reviews/1
   # PATCH/PUT /reviews/1.json
   def update
-    respond_to do |format|
       if @review.update(review_params)
-        format.html { redirect_to @review, notice: "Review was successfully updated." }
-        format.json { render :show, status: :ok, location: @review }
+        redirect_to @review, notice: "Review was successfully updated."
       else
-        format.html { render :edit }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
+        render :edit
       end
-    end
   end
 
   # DELETE /reviews/1
   # DELETE /reviews/1.json
   def destroy
     @review.destroy!
-    respond_to do |format|
-      format.html { redirect_to reviews_url, notice: "Review was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to reviews_url, notice: "Review was successfully destroyed."
   end
 
   private
