f86ae3e1119e66e6b939332e0bf1c910cb82bb98
Shintani Teppei <teppest1215@gmail.com>
2017-08-31 22:35:01 +0900

    bundle exec rubocop -a

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -15,7 +15,7 @@
 #  index_comments_on_user_id    (user_id)
 #
 
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Comment, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
