5523bb69ccd72509105cdfe13cdc0becaf1ff615
Shintani Teppei <teppest1215@gmail.com>
2017-09-01 14:53:04 +0900

    rubocopの指摘を修正

--- db/fixtures/company.rb
+++ db/fixtures/company.rb
@@ -15,6 +15,6 @@ companies_name.each.with_index(1) do |company_name, idx|
     c.name = company_name
     c.url = rand(2).odd? ? "http://hogehoge.com" : nil
     c.address = rand(2).odd? ? "茨城県つくば市天王台１丁目１−１" : nil
-    c.logo = File.open(File.join(Rails.root, "db/fixtures/images/company_logo/#{company_name}.png"))
+    c.logo = File.open(Rails.root.join("db", "fixtures", "images", "company_logo", "#{company_name}.png"))
   end
 end
