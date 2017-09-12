b93e79c8c8e60637257f9e3eb0293971a3aa5fe9
Shintani Teppei <teppest1215@gmail.com>
2017-09-12 09:21:20 +0900

    bundle exec rails g migration add_pdf_to_reviews pdf:string & bundle exec rake db:migtate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170901011825) do
+ActiveRecord::Schema.define(version: 20170912001927) do
 
   create_table "benefits", force: :cascade do |t|
     t.integer "review_id"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20170901011825) do
     t.integer "rate", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "pdf"
     t.index ["company_id"], name: "index_reviews_on_company_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -10,6 +10,7 @@
 #  rate       :integer          not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  pdf        :string
 #
 # Indexes
 #
