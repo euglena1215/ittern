ab5465ae0e56b7eed36250a3b77ec36ed309d6db
i544c <leo@i544c.me>
2017-09-21 13:06:51 +0900

    経験談の交通費、宿泊場所、ランチの表示修正

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,28 @@
 module ReviewsHelper
+  # 交通費支給ありなしの表示
+  def show_transportation(bool)
+    if bool then
+      "支給あり！"
+    else
+      "支給なし..."
+    end
+  end
+
+  # 宿泊場所提供ありなしの表示
+  def show_accommodation(bool)
+    if bool then
+      "提供される！"
+    else
+      "提供されない..."
+    end
+  end
+
+  # ランチ出る出ないの表示
+  def show_lunch(bool)
+    if bool then
+      "出る！"
+    else
+      "出ない..."
+    end
+  end
 end
