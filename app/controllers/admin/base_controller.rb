132d271fb4514766bababb394912183bd2e5edc7
Shintani Teppei <teppest1215@gmail.com>
2017-09-25 14:07:59 +0900

    admin/companiesの枠組みのみ作成/admin用layoutの追加

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,6 +1,8 @@
 class Admin::BaseController < ApplicationController
   before_action :check_admin
 
+  layout 'admin'
+
   def check_admin
     raise StandardError.new "管理者のみ閲覧できるページです" unless current_user.is_admin?
   end
--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,6 @@ Rails.application.routes.draw do
   namespace :admin do
     root "users#index"
     resources :users, except: [:new, :create]
+    resources :companies
   end
 end
