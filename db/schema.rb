6df59d0d6f58dabc0b9bab647e6a9f52bb35cb9b
Shintani Teppei <teppest1215@gmail.com>
2017-09-25 11:55:29 +0900

    userにis_adminを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170901011825) do
+ActiveRecord::Schema.define(version: 20170925023754) do
 
   create_table "benefits", force: :cascade do |t|
     t.integer "review_id"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 20170901011825) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.boolean "is_admin", default: false, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
