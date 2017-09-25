54a85b917920e245707f268748f8b7424eb81c04
Shintani Teppei <teppest1215@gmail.com>
2017-09-25 13:29:37 +0900

    admin/usersをきちんと作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -41,6 +41,6 @@ class Admin::UsersController < Admin::BaseController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.fetch(:user, {})
+      params.require(:user).permit(:name, :is_admin)
     end
 end
