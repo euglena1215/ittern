d02060723a4a59d18fcd0c5567ae3163ef832d2f
i544c <leo@i544c.me>
2017-09-26 19:39:07 +0900

    経験談の詳細画面を整えた
    benefitをアイコンで表示するようにした。
    レイアウトを整えた。

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,28 +1,21 @@
 module ReviewsHelper
+  # ありなしでアイコンの色を変更
+  def icon_color(bool)
+    bool ? "pink" : "grey"
+  end
+
   # 交通費支給ありなしの表示
   def show_transportation(bool)
-    if bool then
-      "支給あり！"
-    else
-      "支給なし..."
-    end
+    bool ? "支給あり" : "支給なし"
   end
 
   # 宿泊場所提供ありなしの表示
   def show_accommodation(bool)
-    if bool then
-      "提供される！"
-    else
-      "提供されない..."
-    end
+    bool ? "提供される！" : "提供されない..."
   end
 
   # ランチ出る出ないの表示
   def show_lunch(bool)
-    if bool then
-      "出る！"
-    else
-      "出ない..."
-    end
+    bool ? "出る！" : "出ない..."
   end
 end
